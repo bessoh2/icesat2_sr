c40b46e1b998effa25c4344bfb9e771ca08e1028
Hannah Besso <71660036+bessoh2@users.noreply.github.com>
2022-04-06 10:12:10 -0700

    Create methow_32610.sh
    Code from Eli Schwat: https://github.com/elischwat/hsfm-geomorph/blob/master/download-dems/baker/create_baker_reference.sh

---
+++ download_dems/methow_32610.sh
@@ -0,0 +1,35 @@
+## Originally from Eli Schwat, supplied February 23rd 2022: https://github.com/elischwat/hsfm-geomorph/blob/master/download-dems/baker/create_baker_reference.sh
+
+#! /bin/bash
+
+# Download these files form the WA DNR LIDAR portal. We use all the tiles available to get maximum coverage.
+# methow_basin_2018_dtm_0.tif
+# ...
+# methow_basin_2018_dtm_38.tif
+
+cd Documents/Documents_Grad/Research/IDS_statewide/datasetsA/methow_basin_2018/dtm/raw
+fn_list=$(ls *.tif)
+# Activate environment containing gdal tools
+conda activate dtm_reproj
+# Convert from survey ft. to meters, outputting all tiff files with suffix _m added
+parallel "gdal_calc.py --co COMPRESS=LZW --co TILED=YES --co BIGTIFF=IF_SAFER --NoDataValue=-9999 --calc 'A*0.3048' -A {} --outfile {.}_m.tif" ::: $fn_list
+# Reproject from ESRI:102749 to EPSG:26910, with suffix _utm added.
+parallel "gdalwarp -co COMPRESS=LZW -co TILED=YES -co BIGTIFF=IF_SAFER -dstnodata -9999 -r cubic -s_srs ESRI:102749 -t_srs EPSG:26910 {} {.}_utm.tif" ::: *_m.tif
+# Activate Ames Stereo Pipeline environment
+conda deactivate
+conda activate asp
+# Transform from NAVD88 datum to WGS84 ellipsoid, no suffix is added
+parallel "dem_geoid --reverse-adjustment {}" ::: *_m_utm.tif
+# Activate environment containing gdal
+conda deactivate
+conda activate datum_reproj
+# Manually edit the metadata (doesn't automatically update with above step) from 26910 to 32610, adds suffix -adj
+parallel "gdal_edit.py -a_srs EPSG:32610 {}" ::: *_m_utm-adj.tif
+# Activate environment containing Ames StereoPipeline
+conda deactivate
+conda activate asp
+# Mosaic the DEMs
+dem_mosaic *_m_utm-adj.tif -o methow_basin_2018_dtm_combined_32610_1m.tif
+
+# Make a low resolution version
+gdalwarp -tr 10 10 -r cubic methow_basin_2018_dtm_combined_32610_1m.tif methow_basin_2018_dtm_combined_32610_12m.tif
